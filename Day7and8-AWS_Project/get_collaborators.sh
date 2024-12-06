711d0a7ba53782c3e5454cd47cb7c18ec227ab62
Mohan Krishna <101246321+ghsmk@users.noreply.github.com>
2024-12-06 11:42:10 +0000

    separate: GITHUB_REPOSITORY as command line argument

--- Day7and8-AWS_Project/get_collaborators.sh
+++ Day7and8-AWS_Project/get_collaborators.sh
@@ -11,8 +11,10 @@ GITHUB_API_URL=https://api.github.com/
 
 # Owner/Organization, Repository and Personal Access Token values are fetched from secret variables
 GITHUB_OWNER=$OWNER
-GITHUB_REPOSITORY=$REPOSITORY
 GITHUB_ACCESS_TOKEN=$ACCESS_TOKEN
+
+# Fetch command line arguments / input from user while running this script
+GITHUB_REPOSITORY=$1
 
 
 # Fetch collaborator details of the github repository
@@ -21,4 +23,4 @@ curl -L \
   -H "Accept: application/vnd.github+json" \
   -H "Authorization: Bearer ${GITHUB_ACCESS_TOKEN}" \
   -H "X-GitHub-Api-Version: 2022-11-28" \
-  https://api.github.com/repos/${GITHUB_OWNER}/${GITHUB_REPOSITORY}/collaborators | jq '.[].login'
+  https://api.github.com/repos/${GITHUB_OWNER}/${GITHUB_REPOSITORY}/collaborators | jq -r '.[].login'
