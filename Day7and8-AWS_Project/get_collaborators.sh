625a33a8c726e63f504151bb7ab2827bc37ae6d8
Mohan Krishna <101246321+ghsmk@users.noreply.github.com>
2024-12-06 11:11:09 +0000

    add: jq

--- Day7and8-AWS_Project/get_collaborators.sh
+++ Day7and8-AWS_Project/get_collaborators.sh
@@ -21,4 +21,4 @@ curl -L \
   -H "Accept: application/vnd.github+json" \
   -H "Authorization: Bearer ${GITHUB_ACCESS_TOKEN}" \
   -H "X-GitHub-Api-Version: 2022-11-28" \
-  https://api.github.com/repos/${GITHUB_OWNER}/${GITHUB_REPOSITORY}/collaborators
+  https://api.github.com/repos/${GITHUB_OWNER}/${GITHUB_REPOSITORY}/collaborators | jq '.[].login'
